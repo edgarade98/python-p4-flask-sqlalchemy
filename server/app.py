a56634223d1725d8f59b310bc9b0d939d5ec0717
Edgar Otieno <eddyade98@gmail.com>
2024-02-29 13:20:39 +0300

    App import Completed

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate
-from models import db
+
+from .models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -12,7 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
-
 
 if __name__ == '__main__':
     app.run(port=5556, debug=True)
